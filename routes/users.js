fe9d89fc6c9aedf9c9c16612093fa5a0a0bcfb23
Austin Kyker <akyker@ebay.com>
2015-06-28 18:26:58 -0700

    completed api

--- routes/users.js
+++ routes/users.js
@@ -36,16 +36,43 @@ router.get('/:id/disliked_users', function(req, res) {
 	});
 });
 
+// POST /users/
+// creates a new user.
+router.post('/', function(req, res){
+	req.graphDB.create('VERTEX', 'User')
+	.set({
+		name: req.body.name,
+		age: req.body.age
+	})
+	.one()
+	.then(function(vertex){
+		res.send(vertex);
+	});
+});
+
+// NOTE:
+// When a POST request is received for a user A to like or dislike
+// a user B, the relations between A and B that already exist are
+// destroyed. The reason is that a user A cannot both like and 
+// dislike a user B.
+
+
 // POST /users/:user_id/like_user/:other_user_id
+// returns the edge that was created.
 router.post('/:id/like_user/:other_user_id', function(req, res){
-	createRelation(req, 'Like').then(function(edge) {
+	destroyRelations(req).then(function(){
+		return createRelation(req, 'Like');
+	}).then(function(edge) {
 		res.send(edge);
 	});
 });
 
 // POST /users/:user_id/dislike_user/:other_user_id
+// returns the edge that was created.
 router.post('/:id/dislike_user/:other_user_id', function(req, res){
-	createRelation(req, 'Dislike').then(function(edge) {
+	destroyRelations(req).then(function(){
+		return createRelation(req, 'Dislike');
+	}).then(function(edge) {
 		res.send(edge);
 	});
 });
@@ -82,5 +109,22 @@ function createRelation(req, type) {
 	.one();
 }
 
+// Any Like or Dislike relation that exists between a user A and a
+// user B is deleted. The reason a Promise is used is because
+// only after the pre-existing relations are deleted should a new
+// relation be created. 
+function destroyRelations(req) {
+	return Promise.all([
+		destroyRelation(req, 'LIKE'),
+		destroyRelation(req, 'Dislike')
+	]);
+}
+
+function destroyRelation(req, type) {
+	return req.graphDB.delete('EDGE', type)
+				.from(getRID(req))
+				.to('#' + req.params.other_user_id)
+				.scalar()
+}
 
 module.exports = router;
